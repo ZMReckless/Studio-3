e65cd355cd3b877430bc9376106ceacedb71aa39
Jay K <rtdx24@gmail.com>
2021-06-16 16:21:22 +1000

    controller model and anims
    added model and animations to controller, along with correlated script changes

--- Assets/Scripts/Jay's Scripting Basement/FPSMovement.cs
+++ Assets/Scripts/Jay's Scripting Basement/FPSMovement.cs
@@ -14,21 +14,39 @@ public class FPSMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    public Animator animator;
+
     Vector3 velocity;
     bool isGrounded;
+    bool isIdle;
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+
+        if (x != 0 ||
+            z != 0)
+        {
+            isIdle = false;
+        }
+        else
+        {
+            isIdle = true;
+        }
+
+        animator.SetBool("isIdle", isIdle);
+
+        animator.SetFloat("Horizontal", x);
+        animator.SetFloat("Vertical", z);
 
         Vector3 move = transform.right * x + transform.forward * z;
 
