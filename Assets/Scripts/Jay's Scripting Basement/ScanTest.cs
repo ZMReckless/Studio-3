ea03b00654a5995d7a68834434f6b0b2fdec023b
Archie-Paxton <67458177+Archie-Paxton@users.noreply.github.com>
2021-07-16 23:27:00 +1000

    Added Scan to mobile controller
    The mobile controller now has a scan button that works. just need to a animation for the cooldown.

--- Assets/Scripts/Jay's Scripting Basement/ScanTest.cs
+++ Assets/Scripts/Jay's Scripting Basement/ScanTest.cs
@@ -17,8 +17,19 @@ public class ScanTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) &&
-            hasScanned == false)
+        //use this for touch input
+        //if (Input.GetKeyDown(KeyCode.Mouse0) &&
+        //    hasScanned == false)
+        //{
+        //    hasScanned = true;
+        //    ScanTimer();
+        //    Scan();
+        //}
+    }
+
+    public void fire()
+    {
+        if (hasScanned == false)
         {
             hasScanned = true;
             ScanTimer();
@@ -32,15 +43,27 @@ public class ScanTest : MonoBehaviour
 
     }
 
-    void Scan()
+    public void Scan()
     {
-        _mousePos = Input.mousePosition;
+        //Use this for touch input
+        //_mousePos = Input.mousePosition;
+        //RaycastHit hit;
+        //Ray castPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        //{
+        //    var scan = Instantiate(scanSphere, (new Vector3(hit.point.x, hit.point.y, hit.point.z)), Quaternion.identity);
+        //    scan.AddComponent<SphereScan>();
+        //}
 
-        Ray castPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //Use this for button press
+
+        Ray castPoint = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
-        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity)){
-            var scan = Instantiate(scanSphere, (new Vector3(hit.point.x, hit.point.y, hit.point.z)), Quaternion.identity);
-            scan.AddComponent<SphereScan>();
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        {
+                var scan = Instantiate(scanSphere, (new Vector3(hit.point.x, hit.point.y, hit.point.z)), Quaternion.identity);
+                scan.AddComponent<SphereScan>();
         }
 
     }
