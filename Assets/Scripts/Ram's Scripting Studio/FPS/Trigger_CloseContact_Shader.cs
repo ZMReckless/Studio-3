b79c20378a995dd167919817b73af58796608779
ramkillnani <ramkhilnani94@gmail.com>
2021-07-22 10:40:06 +1000

    Close Contact Shader completely functional
    Close contact triggers when player gets close, just edit the collider size to easily adjust this.

--- Assets/Scripts/Ram's Scripting Studio/FPS/Trigger_CloseContact_Shader.cs
+++ Assets/Scripts/Ram's Scripting Studio/FPS/Trigger_CloseContact_Shader.cs
@@ -7,7 +7,7 @@ public class Trigger_CloseContact_Shader : MonoBehaviour
     public Material mat;
     private float thresholdValue = 1f;
     private float deTriggerThresholdValue = 0f;
-    private float thresholdChangeAmount = 0.01f;
+    private float thresholdChangeAmount = 0.025f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,17 +18,21 @@ public class Trigger_CloseContact_Shader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Y))
-        {
+        //if(Input.GetKeyDown(KeyCode.Y))
+        //{
             
-            StartCoroutine(TriggerCloseContact());
+        //    StartCoroutine(TriggerCloseContact());
 
-        }
-        if (Input.GetKeyDown(KeyCode.U))
-        {
+        //}
+        //if (Input.GetKeyDown(KeyCode.U))
+        //{
             
-            StartCoroutine(DeTriggerCloseContact());
-        }
+        //    StartCoroutine(DeTriggerCloseContact());
+        //}
+        //if(Input.GetKey(KeyCode.P))
+        //{
+        //    triggertest();
+        //}
     }
 
     IEnumerator TriggerCloseContact()
@@ -37,13 +41,21 @@ public class Trigger_CloseContact_Shader : MonoBehaviour
         while(thresholdValue > 0)
         {
             yield return new WaitForSeconds(0);
-            StopCoroutine(DeTriggerCloseContact());
+            
             thresholdValue -= thresholdChangeAmount;
             mat.SetFloat("Threshold", thresholdValue);
-            
+
+            if (thresholdValue <= 0)
+            {
+                thresholdValue = 0;
+                if(thresholdValue == 0)
+                {
+                    StopAllCoroutines();
+                }
+                
+            }
         }
-        
-        
+       
 
     }
 
@@ -53,12 +65,70 @@ public class Trigger_CloseContact_Shader : MonoBehaviour
         while (deTriggerThresholdValue < 1)
         {
             yield return new WaitForSeconds(0);
-            StopCoroutine(TriggerCloseContact());
-            deTriggerThresholdValue += thresholdChangeAmount;
-            mat.SetFloat("Threshold", deTriggerThresholdValue);
+           
+            thresholdValue += thresholdChangeAmount;
+            mat.SetFloat("Threshold", thresholdValue);
             
+            if(thresholdValue >= 1)
+            {
+                thresholdValue = 1;
+                if(thresholdValue == 1)
+                {
+                    StopAllCoroutines();
+                }
+                
+            }
         }
         
+
     }
+
+    //void triggertest() //use this
+    //{
+    //    thresholdValue -= thresholdChangeAmount;
+    //    mat.SetFloat("Threshold", thresholdValue);
+
+    //    if(thresholdValue <= 0)
+    //    {
+    //        thresholdValue = 0;
+    //    }
+    //    Debug.LogWarning(thresholdValue);
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            StartCoroutine(TriggerCloseContact());
+
+            //thresholdValue -= thresholdChangeAmount;
+            //mat.SetFloat("Threshold", thresholdValue);
+
+            //if (thresholdValue <= 0)
+            //{
+            //    thresholdValue = 0;
+            //}
+
+            //Debug.LogWarning(thresholdValue);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            StartCoroutine(DeTriggerCloseContact());
+            //thresholdValue += thresholdChangeAmount;
+            //mat.SetFloat("Threshold", thresholdValue);
+
+            //if(thresholdValue >= 1)
+            //{
+            //    thresholdValue = 1;
+            //}
+        }
+    }
+
+   
 
 }
