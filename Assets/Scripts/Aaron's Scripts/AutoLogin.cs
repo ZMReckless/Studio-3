fce5c1b40b977a9d3c04ce8f744db7f60a4459f4
Aaron <67357552+AyeAyeRon2@users.noreply.github.com>
2021-07-29 00:18:50 +1000

    added easy sign in for testing
    you set the details then it automatically sets your username and password, dont need to type it again

---
+++ Assets/Scripts/Aaron's Scripts/AutoLogin.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AutoLogin : MonoBehaviour
+{
+    [Header("Login Details")]
+    public string username;
+    public string password;
+
+    [Header("Components")]
+    public TMP_InputField usernameInput;
+    public TMP_InputField passwordInput;
+
+    public void InputDetails()
+    {
+        usernameInput.text = username;
+        passwordInput.text = password;
+    }
+}
