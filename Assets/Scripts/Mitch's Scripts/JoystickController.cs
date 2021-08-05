32bb15ed2d3de301231ec12bd5378ac2e41b5fd0
Mitchell Hocking <1018984@student.sae.edu.au>
2021-08-05 14:23:08 +1000

    Joystick controller

---
+++ Assets/Scripts/Mitch's Scripts/JoystickController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class JoystickController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float deltaX;
+    //public CinemachineVirtualCamera dolly;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPos = touch.position;
+            touchPos.z = 924f;
+            Vector3 vec = Camera.main.ScreenToWorldPoint(touchPos);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    deltaX = vec.x - transform.position.x;
+                    break;
+
+                case TouchPhase.Moved:
+                    rb.MovePosition(new Vector2(Mathf.Clamp(vec.x - deltaX, 60, 200), this.gameObject.transform.position.y));
+                   
+                    break;
+
+                case TouchPhase.Ended:
+                    rb.velocity = Vector2.zero;
+                    break;
+            }
+
+            if (this.gameObject.transform.position.x  < 140)
+            {
+                Debug.Log("Left");
+                //dolly.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition -= speed;
+            }
+            if (this.gameObject.transform.position.x > 140)
+            {
+                Debug.Log("Right");
+                //dolly.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition += speed;
+            }
+
+        }
+    }
+}
