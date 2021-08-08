3c35867087d724d7f1e2e3eec4c68cec80206162
ramkillnani <ramkhilnani94@gmail.com>
2021-08-09 00:43:51 +1000

    Team Icons above player for mobile view
    x

--- Assets/Scripts/Ram's Scripting Studio/Auto_Rotate.cs
+++ Assets/Scripts/Ram's Scripting Studio/Auto_Rotate.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Auto_Rotate : MonoBehaviour
 {
     public float rotationSpeed;
+    public Material team1;
+    public Material team2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(transform.root.gameObject.CompareTag("Team1"))
+        {
+            GetComponent<Renderer>().material = team1;
+        }
+        if (transform.root.gameObject.CompareTag("Team2"))
+        {
+            GetComponent<Renderer>().material = team2;
+        }
     }
 
     // Update is called once per frame
