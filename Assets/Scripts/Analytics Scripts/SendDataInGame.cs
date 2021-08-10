6d6769f0fa3eb8d617b03b69a2c85b03c9c2b2f2
ZMReckless <zachmreckless@outlook.com>
2021-08-10 15:21:15 +1000

    added kill tracking

---
+++ Assets/Scripts/Analytics Scripts/SendDataInGame.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public static class SendDataInGame
+{
+    public static int kills, deaths;
+    static string userPlayFabID;
+    public static void PullData() {
+        GetAccountInfo();
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest() {
+            PlayFabId = userPlayFabID,
+            Keys = null
+        }, result => {
+            Debug.Log("Got user Data");
+            if (result.Data == null || !result.Data.ContainsKey("Kills")) {
+
+            }
+            else {
+                Debug.Log("Kills:" + result.Data["Kills"].Value);
+                kills = System.Convert.ToInt32(result.Data["Kills"].Value);
+                Debug.Log(kills);
+            }
+        }, error => {
+            Debug.Log("Got Error Retrieving User Data:");
+            Debug.Log(error.GenerateErrorReport());
+        });
+    }
+    public static void SendData() {
+        PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest() {
+            Data = new Dictionary<string, string>(){
+                {"Kills", kills.ToString()},
+                {"Deaths", deaths.ToString()}
+            }
+        }, result => Debug.Log("Successfully Update User Data"),
+        error => {
+            Debug.Log("Got Error Updating User Data");
+            Debug.LogError(error.GenerateErrorReport());
+        });
+    }
+    public static void UpdateKillsOrDeaths(int winLose) {
+        if (winLose == 1) {
+            kills++;
+        }
+        else {
+            deaths++;
+        }
+        SendData();
+    }
+    #region GetUserID
+    static void GetAccountInfo() {
+        GetAccountInfoRequest request = new GetAccountInfoRequest();
+        PlayFabClientAPI.GetAccountInfo(request, Success, Fail);
+    }
+    static void Success(GetAccountInfoResult result) {
+        userPlayFabID = result.AccountInfo.PlayFabId;
+    }
+    static void Fail(PlayFabError error) {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+    #endregion
+}
